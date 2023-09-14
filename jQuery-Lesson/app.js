e14fab3f108d5944308c5b3a6f73cc9bfd3e47aa
yonbok <na.re.zutomayo14@gmail.com>
2023-09-14 04:06:57 +0000

    簡単なスライドショー装備

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,6 +1,28 @@
-$(document).ready(function () {
-  $("body").html("<h1>Hello jQuery!!</h1>");
+// 下から上にスライド
+$(function(){
+  $(".box1").slideUp();
 });
+
+
+// 上から下にスライド
+// $(function(){
+//   $(".box1").slideDown();
+// });
+
+
+// boxを表示させる
+// $(function(){
+//   $(".box1").css({
+//     "background-color": "#0000FF",
+//     "height": "100px"
+//   });
+// });
+
+
+
+// $(document).ready(function () {
+//   $("body").html("<h1>Hello jQuery!!</h1>");
+// });
 
 
 // 省略形
