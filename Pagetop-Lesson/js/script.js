21a3768441d8acae2a62f1856d65a49ac859814e
yonbok <na.re.zutomayo14@gmail.com>
2023-09-22 10:29:36 +0000

    ハンバーガーメニュー用のファイル新規作成完了

--- Pagetop-Lesson/js/script.js
+++ Pagetop-Lesson/js/script.js
@@ -3,8 +3,11 @@ $(function(){
     // 上の引数で使われているeventは省略も可能　例：function(e)
     // 以下は#back内のaタグがクリックされたときの処理
     $("body, html").animate({
-      scrollTop:0
-    }, 800);
-    event.preventDefault();
+      scrollTop:0 //変化対象のプロパティ名：変化値
+      //上の記述は「最上部に移動する」の意味
+    }, 800);  //アニメーションの動作時間
+    //上の記述は800ミリ秒間(0.8秒間)かけてページの最上部まで移動する」の意味
+    //ミリ秒指定以外にもshow,normal,fastで指定することも可能
+    event.preventDefault(); //aタグ機能を無効にするメソッド
   });
 });
