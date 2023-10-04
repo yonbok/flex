1015a45ddd34ad4bd9c075ebc3e3dcbd23390917
reia <reia@LAPTOP-06INNEGK>
2023-10-05 00:58:19 +0900

    SamuraiEngineer出力完了

--- JavaSample/src/sample/JavaSampleClass.java
+++ JavaSample/src/sample/JavaSampleClass.java
@@ -3,8 +3,18 @@ package sample;
 public class JavaSampleClass {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-       System.out.println("Hello Java!!");
+		//クラスのオブジェクトを生成
+		TestClass cl = new TestClass();
+		
+		//メンバ変数を参照する
+		String str1 = cl.name;
+		
+		//メソッドの呼び出し
+		String str2 = cl.TestMethod();
+		
+		//結果を出力
+		System.out.println(str1 + str2);
+		
 	}
 
 }
