46adb9cc38a07919bd9fca6c01260f86599d3b9e
reia <reia@LAPTOP-06INNEGK>
2023-10-05 23:23:00 +0900

    printとprintlnの違いを確認

---
+++ JavaSample/src/sample/TestClass.java
@@ -0,0 +1,21 @@
+package sample;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// 数値の表示
+//		System.out.print(123);
+//		System.out.println(456);
+		//　文字列の表示
+//		System.out.print("abc");
+//		System.out.println("def");
+		
+		// print()では、()内のものを表示した後改行しない
+		// println()はその逆
+		
+		
+		System.out.println(1 + " + " + 1 + " = " + (1+1));
+		System.out.println(2 + "+" + 3 + "=" + (2+3));
+	}
+
+}
