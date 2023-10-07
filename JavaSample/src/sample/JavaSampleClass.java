0645b11a5f28e9cdd2f2243a8606fe3af1411e09
yonbok <na.re.zutomayo14@gmail.com>
2023-10-07 16:11:40 +0900

    修正

--- JavaSample/src/sample/JavaSampleClass.java
+++ JavaSample/src/sample/JavaSampleClass.java
@@ -2,19 +2,26 @@ package sample;
 
 public class JavaSampleClass {
 
+	private String name;
+
 	public static void main(String[] args) {
 		//クラスのオブジェクトを生成
-		TestClass cl = new TestClass();
+		JavaSampleClass cl = new JavaSampleClass();
 		
 		//メンバ変数を参照する
 		String str1 = cl.name;
 		
 		//メソッドの呼び出し
-		String str2 = cl.TestMethod();
+		String str2 = cl.JavaSampleMethod();
 		
 		//結果を出力
 		System.out.println(str1 + str2);
 		
+	}
+
+	private String JavaSampleMethod() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
 	}
 
 }
