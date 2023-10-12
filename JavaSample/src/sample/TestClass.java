dfae22941fb9431e3f0cdb9cfc51c40f447f7990
yonbok <na.re.zutomayo14@gmail.com>
2023-10-13 00:28:55 +0900

    合計値と平均値表示

--- JavaSample/src/sample/TestClass.java
+++ JavaSample/src/sample/TestClass.java
@@ -14,14 +14,26 @@ public class TestClass {
 		// println()はその逆
 		
 		
-		System.out.println(1 + " + " + 1 + " = " + (1+1));
-		System.out.println(2 + "+" + 3 + "=" + (2+3));
-		System.out.println(1 + "+" + 2 + "=" + (1+2));
-	}
+//		System.out.println(1 + " + " + 1 + " = " + (1+1));
+//		System.out.println(2 + "+" + 3 + "=" + (2+3));
+//		System.out.println(1 + "+" + 2 + "=" + (1+2));
+//	}
+//	
 
-	public String TestMethod() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		double[] d = new double[3];
+		double sum,avg;
+		d[0] = 1.2;
+		d[1] = 3.7;
+		d[2] = 4.1;
+		sum = 0.0;
+		for(int i = 0; i < d.length; i++) {
+			System.out.println(d[i] + "");
+			sum += d[i];
+		}
+		System.out.println();
+		avg = sum / 3.0;
+		System.out.println("合計値：" + sum);
+		System.out.println("平均値：" + avg);
 		
 	}
 
