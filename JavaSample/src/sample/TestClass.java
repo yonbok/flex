868c36a7162413502eb9650fcd38cbcf0fb9902b
yonbok <na.re.zutomayo14@gmail.com>
2023-10-13 23:44:57 +0900

    配列の初期化

--- JavaSample/src/sample/TestClass.java
+++ JavaSample/src/sample/TestClass.java
@@ -35,6 +35,20 @@ public class TestClass {
 		System.out.println("合計値：" + sum);
 		System.out.println("平均値：" + avg);
 		
+		
+		int n[] = { 5,4,3,2,1 };
+		String s[] = {"ABC", "DEF", "GHI"};
+		int i;
+		//整数型配列変数nの成分表示
+		for(i = 0; i < n.length; i++) {
+			System.out.print(n[i] + " ");
+		}
+		System.out.println();
+		//文字列型配列変数nの成分表示
+		for(i = 0;i < s.length; i++) {
+			System.out.print(s[i] + " ");
+		}
+		System.out.println();
 	}
 
 }
