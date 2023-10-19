369c411feba3e8dc8105831a9f2bb62fa5034f01
yonbok <na.re.zutomayo14@gmail.com>
2023-10-19 23:03:58 +0900

    else ifを使った条件分岐の問題途中

--- JavaSample/src/sample/SubSample.java
+++ JavaSample/src/sample/SubSample.java
@@ -17,6 +17,34 @@ public class SubSample {
 			System.out.println("10以下か90以上の値です。");
 		}else
 			System.out.println("範囲外です。");
+		
+		
+		
+		int book = (int)(Math.random()*10)+1;
+		int text = (int)(Math.random()*10)+1;
+		System.out.println("本の数：" + book);
+		System.out.println("教科書の数：" + text);
+		if(book > text) {
+			System.out.println("本の数の方が多いです。");
+		}else if(text > book){
+			System.out.println("教科書の数の方が多いです。");
+		}else{
+			System.out.println("本の数と教科書の数は同じです。");
+		}
+		System.out.println();
+		
+		
+		
+		int group = (int)(Math.random()*100)+1;
+		System.out.println("グループ数：" + group);
+		if(group % 2 == 0) {
+			System.out.println("2の倍数です。");
+		}else if(group % 3 == 0) {
+			System.out.println("3の倍数です。");
+		}else if(group % 2 ==0 && group % 3 == 0) {
+			System.out.println("2と3の公倍数です。");
+		}else
+			System.out.println("範囲外です。");
 	}
 
 }
